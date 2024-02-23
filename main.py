b9991530ac5a5035b04b9e95950a12d962b6a270
AndreyggWER <an1476508@gmail.com>
2024-02-23 13:07:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import time
+
+app = FastAPI()
+now = time.time()
+LocalTimer = time.localtime(now)
+Curh = 2 #LocalTimer.tm_hour
+@app.get("/andrey")
+
+def HellWorld():
+    if Curh >= 6 and Curh <= 11:
+        return "Доброе утро, Андрей"
+    elif Curh >= 12 and Curh <= 17:
+        return "Добрый день, Андрей"
+    elif Curh >= 18 and Curh <= 22:
+        return "Добрый вечер, Андрей"
+    else:
+        return "Доброй ночи, Андрей"
